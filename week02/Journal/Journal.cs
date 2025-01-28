c92c29f76fbb0d4eeafb52dba987704d9b746972
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-01-28 18:26:48 +0300

    mjkkkiuihy

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,5 +1,62 @@
-1/24/2025|If I had one thing I could do over today, what would it be?|Eating
-1/24/2025|How did I see the hand of the Lord in my life today?|When I was sick
-1/24/2025|If I had one thing I could do over today, what would it be?|when I was sick
-1/24/2025|What was the strongest emotion I felt today?|Iccident
-1/24/2025|What was the best part of my day?|when I won racing 
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Represents a journal containing multiple entries.
+/// </summary>
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    /// <summary>
+    /// Adds a new entry to the journal.
+    /// </summary>
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    /// <summary>
+    /// Displays all entries in the journal.
+    /// </summary>
+    public void DisplayAll()
+    {
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    /// <summary>
+    /// Saves all journal entries to a file.
+    /// </summary>
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (var entry in _entries)
+            {
+                outputFile.WriteLine(entry.ToCsvFormat());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Loads journal entries from a file, replacing current entries.
+    /// </summary>
+    public void LoadFromFile(string fileName)
+    {
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(fileName);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            if (parts.Length == 3)
+            {
+                Entry entry = new Entry(parts[0], parts[1], parts[2]);
+                _entries.Add(entry);
+            }
+        }
+    }
+}
