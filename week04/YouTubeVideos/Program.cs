15023ed1a5385b540e6615954b1c6b0fc33c65f8
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-02-02 16:38:33 +0300

    General

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,31 @@
-using System;
-
+// Program Execution
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        // Creating videos
+        Video video1 = new Video("C# Basics", "Code Masters", 300);
+        video1.AddComment(new Comment("Michael", "Fantastic tutorial!"));
+        video1.AddComment(new Comment("Sophia", "This was really helpful!"));
+        video1.AddComment(new Comment("Liam", "Clear and concise explanation."));
+
+        Video video2 = new Video("Advanced C#", "Tech Academy", 600);
+        video2.AddComment(new Comment("Olivia", "Loved the examples!"));
+        video2.AddComment(new Comment("Noah", "Great in-depth analysis."));
+        video2.AddComment(new Comment("Ava", "Best tutorial so far!"));
+
+        Video video3 = new Video("C# OOP Concepts", "Dev Pro", 450);
+        video3.AddComment(new Comment("Ethan", "Really insightful content."));
+        video3.AddComment(new Comment("Isabella", "Super useful, thanks!"));
+        video3.AddComment(new Comment("Mason", "Helped me understand OOP better!"));
+
+        // Storing videos in a list
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+
+        // Displaying video details
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
     }
 }
---
+++ week04/YouTubeVideos/video.cs
@@ -0,0 +1,38 @@
+// Video Class
+class Video
+{
+    private string title;
+    private string author;
+    private int lengthInSeconds;
+    private List<Comment> comments;
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        this.title = title;
+        this.author = author;
+        this.lengthInSeconds = lengthInSeconds;
+        this.comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return comments.Count;
+    }
+
+    public void DisplayVideoDetails()
+    {
+        Console.WriteLine($"Title: {title}\nAuthor: {author}\nLength: {lengthInSeconds} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in comments)
+        {
+            Console.WriteLine("- " + comment.GetCommentDetails());
+        }
+        Console.WriteLine();
+    }
+}
