ef9051b1edfba519ada6165df09ed5a8486a2841
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-02-13 00:45:03 +0300

    yesu

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Provide a getter method for the student's name if needed
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
